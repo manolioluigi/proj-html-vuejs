7ced3f52a6d513b2642f3847afc200cdeb2a7b7f
manolioluigi <luigi.manolio1999@gmail.com>
2023-01-25 14:46:07 +0100

    footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -16,6 +16,9 @@
 
 <template>
     <div class="container-fluid no-padding">
+
+        <!--prima riga-->
+
         <div class="row my-5 width-full no-margin-sides">
 
             <div class="col-12 no-padding">
@@ -33,6 +36,50 @@
             </div>
 
         </div>
+
+        <!--seconda riga-->
+
+        <div class="row mb-5">
+
+            <div class="col-7 d-flex flex-column justify-content-center align-items-start px-5 mb-5">
+
+                <img src="../assets/img/avada-bakery-logo.png" alt="">
+                <div class="my-3 navbar d-flex flex-column">
+                    <div class="d-flex justify-content-between width-full">
+                        <a href="#">Shop</a>
+                        <a href="#">About</a>
+                        <a href="#">Gallery</a>
+                        <a href="#">Locations</a>
+                        <a href="#">Journal</a>
+                    </div>
+                    <div class="d-flex justify-content-between width-full shifted">
+                        <a href="#">Contact</a>
+                        <a href="#">Orders</a>
+                    </div>
+                </div>
+                <span class="my-2 darkgray font-size-12">© Copyright 2019 | Avada Theme by ThemeFusion | All Rights Reserved | Powered by WordPress</span>
+
+            </div>
+
+            <div class="col-5 text-center">
+
+                <h3 class="purple">Subscribe to our Newsletter</h3>
+                <div class="email mt-3">
+                    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email addess*">
+                    <div class="purple-button">Subscribe</div>
+                </div>
+                <div class="social">
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-twitter mx-3"></i>
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-pinterest mx-3"></i>
+                    <i class="fa-brands fa-youtube"></i>
+                </div>
+
+            </div>
+
+        </div>
+
     </div>
 </template>
 
@@ -60,10 +107,20 @@
         }
     }
 
+    a{
+        color: $purple;
+    }
+
     h1{
         font-size: 50px;
         font-weight: bold;
         color: $white;
+        font-family: 'Source Serif Pro', serif;
+    }
+
+    h3{
+        font-size: 25px;
+        font-weight: bold;
         font-family: 'Source Serif Pro', serif;
     }
 
@@ -74,6 +131,41 @@
         padding: 15px 40px;
         border-radius: 10px;
         font-size: 18px;
+    }
+
+    .purple-button{
+        background-color: $purple;
+        color: $white;
+        width: fit-content;
+        padding: 10px 25px;
+        border-radius: 10px;
+    }
+
+    .col-7 img{
+        width: 150px;
+    }
+
+    .email{
+        display: flex;
+        input{
+            width: 60%;
+        }
+    }
+
+    .social{
+        display: flex;
+        justify-content: end;
+        margin: 10% 13% 0 0;
+        font-size: 25px;
+        color: $purple;
+    }
+
+    .navbar{
+        width: 80%;
+        .shifted{
+            margin-top: 10px;
+            margin-left: 40px;
+        }
     }
 
 </style>
